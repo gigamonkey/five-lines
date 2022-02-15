52e3f73a6df06cc1a424fdb647f9494ac4751fe8
Peter Seibel <peter@gigamonkeys.com>
2022-02-14 22:36:58 -0800

    Consistent style for binding event handlers.

--- index.ts
+++ index.ts
@@ -188,10 +188,10 @@ const UP_KEY = "ArrowUp";
 const RIGHT_KEY = "ArrowRight";
 const DOWN_KEY = "ArrowDown";
 
-window.addEventListener("keydown", e => {
+window.onkeydown = e => {
   if (e.key === LEFT_KEY || e.key === "a") inputs.push(Input.LEFT);
   else if (e.key === UP_KEY || e.key === "w") inputs.push(Input.UP);
   else if (e.key === RIGHT_KEY || e.key === "d") inputs.push(Input.RIGHT);
   else if (e.key === DOWN_KEY || e.key === "s") inputs.push(Input.DOWN);
-});
+};
 
